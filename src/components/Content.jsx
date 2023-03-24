e4e5c400f5eab636b8060697c565822489b67e9f
LaNinja06 <87137526+LaNinja06@users.noreply.github.com>
2023-03-24 00:52:59 -0600

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,7 +5,7 @@ export default function Content(){
             <h2>About</h2>
             <p>I used to work as a Network Support Engineer. Now I'm enjoying coding and making cool apps and websites.</p>
             <h2>Interests</h2>
-            <p>Dogs. Food. Travel. Tech. Gadgets. Sports. </p>
+            <p>Dogs. Food. Travel. Tech. Gadgets. Sports. Motorcycles. Cars. </p>
         </div>
     )
 }
