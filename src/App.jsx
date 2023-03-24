03218c1d29650c29f94b225393a64fb99769572a
LaNinja06 <87137526+LaNinja06@users.noreply.github.com>
2023-03-23 23:44:56 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Header from '/src/components/Header'
+import Content from '/src/components/Content'
+import Footer from '/src/components/Footer'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+
+export default function Content(){
+    return (
+        <div className="content">
+            <h2>About</h2>
+            <p>I used to work as a Network Support Engineer. Now I'm enjoying coding and making cool apps and websites.</p>
+            <h2>Interests</h2>
+            <p>Dogs. Food. Travel. Tech. Gadgets. Sports. </p>
+        </div>
+    )
+}
